5b7dc97277acfc70d14a7f70ff0314fed8f5b420
Mark Jason Dominus (陶敏修) <mjd@plover.com>
2019-12-04 15:09:59 -0500

    remove unfinished work-in-progress from WC.hs

--- mjd/chapter-01/WC.hs
+++ mjd/chapter-01/WC.hs
@@ -7,25 +7,12 @@ import Control.Monad (foldM, liftM)
 
 perl_input =
   do
-    args <- getFileArgs
+    args <- getArgs
     case args of
       [] -> hGetContents stdin
       a  -> foldM appendfile "" a  where
         appendfile s f = readFile f >>= return . (s ++)
 
 
-startsWith x (h:_) = h == x
-startsWith x [] = False
-
-getFileArgs = liftM (dropWhile (startsWith '-')) getArgs
-getFlags    = liftM (splitFlags . (takeWhile (startsWith '-'))) getArgs
-  where
-    splitFlags :: String -> [String]
-    splitFlags f@['-', x] = [f]
-    splitFlags ('-':x:y) = "-x" : splitFlags ('-' : y)
-
-
-
 main = do
-  
   perl_input >>= print . length
