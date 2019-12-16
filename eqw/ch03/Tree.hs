2d2ba5ddce6c721c788c6c8133a2aba7b653cc6f
Eric Weinstein <ericw@ziprecruiter.com>
2019-12-16 10:14:17 -0800

    Add EQW solutions to Real World Haskell, Chapter 3 (most of them, anyway)

---
+++ eqw/ch03/Tree.hs
@@ -0,0 +1,4 @@
+-- 2. Define a tree type that has only one constructor, like our Java example.
+-- Instead of the Empty constructor, use the Maybe type to refer to a node's children.
+data Tree a = Node a (Maybe (Tree a)) (Maybe (Tree a))
+  deriving Show
