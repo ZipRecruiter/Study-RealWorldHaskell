8a1b7d2f0f1722c1634c16b9b7b1688da58ee8d7
Mark Jason Dominus (陶敏修) <mjd@plover.com>
2019-12-17 11:35:43 -0500

    experiments with myLength and the FMR

--- mjd/chapter-03/myLength.hs
+++ mjd/chapter-03/myLength.hs
@@ -1,9 +1,26 @@
+-- {-# LANGUAGE NoMonomorphismRestriction #-}
 
+myLength0 ls = foldr (const (+ 1)) 0 ls
 
-myLength :: [a] -> Int
-myLength [] = 0
-myLength (_:as) = 1 + myLength as
+myLength1 :: [a] -> Int
+myLength1 ls = foldr (const (+ 1)) 0 ls
 
-myLength' :: [a] -> Int
-myLength' = foldr (const (+ 1))  0
+myLength2 :: Foldable f => f a -> Int
+myLength2 ls = foldr (const (+ 1))  0 ls
+
+myLength3 :: (Foldable f, Integral b) => f a -> b
+myLength3 ls = foldr (const (+ 1))  0 ls
+
+-- myLength4 :: (Foldable t, Num b) => t a -> b
+myLength4 = foldr (const (+ 1))  0
+
+myLength5 :: [a] -> Int
+myLength5 = foldr (const (+ 1))  0
+
+myLength6 :: Foldable f => f a -> Int
+myLength6 = foldr (const (+ 1))  0
+
+myLength7 :: (Foldable f, Integral b) => f a -> b
+myLength7 = foldr (const (+ 1))  0
+
 
