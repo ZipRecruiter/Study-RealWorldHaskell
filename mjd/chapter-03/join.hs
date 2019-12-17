2fa5e5fa0db0534c8cf9857bb78b777e1214e98c
Mark Jason Dominus (陶敏修) <mjd@plover.com>
2019-12-17 14:56:34 -0500

    trying to use folds to do joins

--- mjd/chapter-03/join.hs
+++ mjd/chapter-03/join.hs
@@ -3,3 +3,14 @@ intersperse sep [] = ""
 intersperse sep (a:rest) 
   | rest == []    =  a
   | otherwise     =  a ++ sep ++ intersperse sep rest
+
+
+-- doesn't work
+-- intersperse2 sep ls = foldr (\a b -> a ++ sep ++ b) "" ls
+
+-- fails on empty list
+-- perlmap f ls = concat $ map f ls
+-- intersperse3 sep ls = concat $ tail $ perlmap (\x -> [ sep, x ]) ls
+
+-- works on nonempty lists
+intersperse4 sep = foldl1 (\a b -> a ++ sep ++ b)
