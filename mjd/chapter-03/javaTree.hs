e8006d5cad5b0791f4519525342348946b4da684
Mark Jason Dominus (陶敏修) <mjd@plover.com>
2019-12-17 11:34:54 -0500

    treeheights for Maybe branch trees

--- mjd/chapter-03/javaTree.hs
+++ mjd/chapter-03/javaTree.hs
@@ -3,6 +3,16 @@ data Tree a = Node { d :: a, left :: Maybe (Tree a), right :: Maybe (Tree a) }
   deriving Show
 
 
+treeheight t = treeheight' (Just t) where
+  treeheight' Nothing = 0
+  treeheight' (Just (Node {left=lt, right=rt})) = 1 + max (treeheight' lt) (treeheight' rt)
+
+treerec op zero t = treerec' (Just t) where
+  treerec' Nothing = zero
+  treerec' (Just (Node { left=lt, right=rt })) = treerec' lt `op` treerec' rt
+
+treeheight2 = treerec (\l r -> 1 + max l r) 0 
+
 aTree = Node 3 (Just (Node 1 Nothing (Just (Node 2 Nothing Nothing)))) Nothing
 
 inorder (Node d lt rt) = inorder' lt ++ [d] ++ inorder' rt
