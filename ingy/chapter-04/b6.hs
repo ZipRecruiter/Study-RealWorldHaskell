5077b7756528e23c4a482c2d9b73585ed2a7de6b
Ingy döt Net <ingy@ingy.net>
2019-12-27 08:46:40 -0800

    Start of another exercise

---
+++ ingy/chapter-04/b6.hs
@@ -0,0 +1,16 @@
+any' f = foldr (\x a -> a || f x ) False
+
+cycle' xs = foldr step [] xs
+  where
+  step x a
+    | (length a) `mod` (length xs) == 0 = a ++ cycle xs
+    | otherwise = x:a
+
+test = do
+  print "any"
+  print $ any' odd [1..5]
+  print $ any' even [1,3..5]
+
+  --WIP cycle' loops forever
+  --print "cycle"
+  --print $ take 10 $ cycle' [1..3]
